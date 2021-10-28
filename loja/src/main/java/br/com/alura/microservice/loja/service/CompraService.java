a50a4caadcfc7538799573914990968a24ec792d
Davi <davi.mirandagonc@gmail.com>
2021-10-27 22:14:04 -0300

    Agora utilizando o Spring feign

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,10 +1,9 @@
 package br.com.alura.microservice.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.dto.CompraDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 
@@ -12,14 +11,13 @@ import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 public class CompraService {
 
 	@Autowired
-	private RestTemplate client;
+	private FornecedorClient fornecedorClient;
 	
 	public void realizaCompra(CompraDTO compra) {
 		
-		var exchange = client.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedorDTO.class );
-		
-		System.out.println(exchange.getBody().getEndereco());
-		
+		InfoFornecedorDTO fornecedor = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+
+		System.out.println(fornecedor);
 	}
 
 }
