67e2529a7101fb14b7329c1a056ef543a18827f6
Davi <davi.mirandagonc@gmail.com>
2021-10-28 21:55:51 -0300

    Aula 5 - FIM

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,14 +11,20 @@ import br.com.alura.microservice.loja.model.Compra;
 
 @Service
 public class CompraService {
+	
+	private static final Logger log = LoggerFactory.getLogger(CompraService.class);
 
 	@Autowired
 	private FornecedorClient fornecedorClient;
 	
 	public Compra realizaCompra(CompraDTO compra) {
 		
-//		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		final String estado = compra.getEndereco().getEstado();
 		
+		log.info("buscando informações do fornecedor de {}", estado);
+		var info = fornecedorClient.getInfoPorEstado(estado);
+		
+		log.info("realizando um pedido");
 		var pedido = fornecedorClient.realizaPedido(compra.getItens());
 
 		Compra compraSalva = new Compra();
